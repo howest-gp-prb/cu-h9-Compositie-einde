1a129fb7d2ffcd01a6dc526efb9ef3994d0acf63
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2021-09-22 08:56:05 +0200

    Add project files.

---
+++ Prb.ClassesAndObjects.Core/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prb.ClassesAndObjects.Core
+{
+    public class Car 
+    {
+        private int topSpeed;
+
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public CarType CarType { get; set; }
+        public int TopSpeed
+        {
+            get { return topSpeed; }
+            set
+            {
+                if (value < 0)
+                    topSpeed = 0;
+                else
+                    topSpeed = value;
+            }
+        }
+
+        public Car()  
+        {
+        }
+        public override string ToString()
+        {
+            return $"{Brand} - {CarType}";
+        }
+    }
+
+}
---
+++ Prb.ClassesAndObjects.Core/CarType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prb.ClassesAndObjects.Core
+{
+    public class CarType
+    {
+        public string TypeName { get; set; }
+        public bool IsCommercial { get; set; }
+        public CarType(string typeName, bool isCommercial)
+        {
+            TypeName = typeName;
+            IsCommercial = isCommercial;
+        }
+        public override string ToString()
+        {
+            string symbol = "NC";  // NC = not commercial
+            if (IsCommercial)
+                symbol = "C"; // C = commercial;
+            return $"{TypeName} - {symbol}";
+        }
+    }
+}
